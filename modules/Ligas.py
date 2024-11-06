47232aa52d8f84ccb390900cd109b518297936c7
rafaelmax <rafaelpjmp@gmail.com>
2024-11-06 16:11:59 -0300

    atualização da base

--- modules/Ligas.py
+++ modules/Ligas.py
@@ -2,5 +2,7 @@ ligas = [
     'brasil/brasileirao-betano', 'brasil/serie-b', 'brasil/serie-c', 'argentina/torneo-betano', 'bolivia/divisao-profissional',
     'chile/primera-division', 'armenia/premier-league', 'australia/liga-a', 'austria/bundesliga', 'belgica/liga-jupiler',
     'belgica/challenger-pro-league', 'bulgaria/parva-liga', 'chile/primeira-divisao', 'china/superliga', 'colombia/primera-a',
-    'croacia/hnl', 'republica-tcheca/chance-liga', 'dinamarca/superliga'
+    'croacia/hnl', 'republica-tcheca/chance-liga', 'dinamarca/superliga', 'equador/liga-pro', 'egito/primeira-liga',
+    'inglaterra/campeonato-ingles', 'inglaterra/2-divisao', 'inglaterra/liga-1', 'inglaterra/liga-2', 'estonia/meistriliiga',
+    'finlandia/veikkausliiga'
 ]  
