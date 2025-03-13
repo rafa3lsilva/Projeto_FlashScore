c328eee62754bdc84967d34b0beb69a360c936c8
rafaelmax <rafaelpjmp@gmail.com>
2025-03-13 16:12:09 -0300

    Atualização da base

--- clear_data.py
+++ clear_data.py
@@ -7,10 +7,12 @@ df = pd.DataFrame(colecao_partidas1.all())
 
 df["Date"] = pd.to_datetime(df["Date"], dayfirst=True)
 df = df.sort_values(["Date", "Time"], ascending=True)
+flt = df["Season"] == "2020/2021", "2022/2023", "2023/2024","2024/2025"
+df = df[flt]
 
 df = df.dropna()
 df = df.reset_index(drop=True)
 df.index += 1
 
 
-df.to_csv('base_excel/base_flashscore.csv',index=False)
+df.to_csv('base_excel/base_.csv',index=False)
